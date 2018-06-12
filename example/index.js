6b0a93c4c88f5a8dda2883cfaa9a82d500cf4f62
Denys Bohdan <Denys_Bohdan@epam.com>
2018-06-12 12:06:37 +0300

    add simple synth module

--- example/index.js
+++ example/index.js
@@ -2,11 +2,105 @@ const SimpleSynth = require('../src/simple-synth/simple-synth');
 
 const startBtn = document.querySelector('#start-btn');
 const stopBtn = document.querySelector('#stop-btn');
+const stopAllBtn = document.querySelector('#stop-all-btn');
+let stop;
+const pressedKeys = [];
 
 startBtn.addEventListener('click', () => {
-  SimpleSynth.play(440);
+  stop = SimpleSynth.play('C#5');
 });
 
 stopBtn.addEventListener('click', () => {
+  stop();
+});
 
+stopAllBtn.addEventListener('click', () => {
+  SimpleSynth.stopAll();
+});
+
+document.addEventListener('keydown', (e) => {
+  if (pressedKeys.indexOf(e.key) !== -1) {
+    return;
+  }
+  switch (e.key) {
+    case 'a':
+      SimpleSynth.play('c5');
+      break;
+    case 's':
+      SimpleSynth.play('d5');
+      break;
+    case 'd':
+      SimpleSynth.play('e5');
+      break;
+    case 'f':
+      SimpleSynth.play('f5');
+      break;
+    case 'g':
+      SimpleSynth.play('g5');
+      break;
+    case 'h':
+      SimpleSynth.play('a5');
+      break;
+    case 'j':
+      SimpleSynth.play('b5');
+      break;
+    case 'w':
+      SimpleSynth.play('c#5');
+      break;
+    case 'e':
+      SimpleSynth.play('d#5');
+      break;
+    case 't':
+      SimpleSynth.play('f#5');
+      break;
+    case 'y':
+      SimpleSynth.play('g#5');
+      break;
+    case 'u':
+      SimpleSynth.play('a#5');
+      break;
+  }
+  pressedKeys.push(e.key);
+});
+
+document.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'a':
+      SimpleSynth.stop('c5');
+      break;
+    case 's':
+      SimpleSynth.stop('d5');
+      break;
+    case 'd':
+      SimpleSynth.stop('e5');
+      break;
+    case 'f':
+      SimpleSynth.stop('f5');
+      break;
+    case 'g':
+      SimpleSynth.stop('g5');
+      break;
+    case 'h':
+      SimpleSynth.stop('a5');
+      break;
+    case 'j':
+      SimpleSynth.stop('b5');
+      break;
+    case 'w':
+      SimpleSynth.stop('c#5');
+      break;
+    case 'e':
+      SimpleSynth.stop('d#5');
+      break;
+    case 't':
+      SimpleSynth.stop('f#5');
+      break;
+    case 'y':
+      SimpleSynth.stop('g#5');
+      break;
+    case 'u':
+      SimpleSynth.stop('a#5');
+      break;
+  }
+  pressedKeys.splice(pressedKeys.indexOf(e.key), 1);
 });
---
+++ src/simple-synth/simple-synth.js
@@ -0,0 +1,112 @@
+const constants = require('./constants');
+
+const SimpleSynth = (function() {
+  const defaultSettings = {
+    type: 'sine',
+    fadeTime: 0.05,
+  };
+  let mergedSettings = {};
+  let context = null;
+
+  const playedNotes = [];
+
+  const _init = () => {
+    try {
+      window.AutioContext = window.AutioContext || window.webkitAudioContext;
+      context = new AudioContext();
+      mergedSettings = Object.assign({}, defaultSettings);
+    } catch (e) {
+      throw new Error('Web Audio API is not supported in this browser!');
+    }
+  };
+
+  const isStringRepresentation = (note) => {
+    return typeof note === 'string';
+  };
+
+  const isNumericRepresentation = (note) => {
+    return typeof note === 'number';
+  };
+
+  const getNoteFrequency = (note) => {
+    const noteLowerCase = note.toLowerCase();
+    let freq = null;
+    Object.keys(constants.NOTES).forEach(key => {
+      if (key === noteLowerCase) {
+        freq = constants.NOTES[key];
+      }
+    });
+    if (!freq) {
+      throw new Error('Note is not supported!');
+    }
+    return freq;
+  };
+
+  const init = (settings) => {
+    mergedSettings = Object.assign({}, defaultSettings, settings);
+  };
+
+  const saveNote = (freq, gain) => {
+    const g = gain;
+    playedNotes.push({
+      freq,
+      gain: g,
+    });
+  };
+
+  const play = (note) => {
+    const gain = context.createGain();
+    gain.gain.value = 0.2;
+    const oscillator = context.createOscillator();
+    oscillator.connect(gain);
+    oscillator.type = mergedSettings.type;
+    let frequency;
+    if (isStringRepresentation(note)) {
+      frequency = getNoteFrequency(note);
+    } else if (isNumericRepresentation(note)) {
+      frequency = note;
+    } else {
+      throw new Error('Note is neither a string nor a number!');
+    }
+    oscillator.frequency.value = frequency;
+    gain.connect(context.destination);
+    oscillator.start();
+    saveNote(frequency, gain);
+    return () => {
+      gain.gain.setTargetAtTime(0, context.currentTime, mergedSettings.fadeTime);
+    };
+  };
+
+  const stopAll = () => {
+    playedNotes.forEach(note => {
+      note.gain.gain.setTargetAtTime(0, context.currentTime, mergedSettings.fadeTime);
+    });
+  };
+
+  const stop = (note) => {
+    let frequency;
+    if (isStringRepresentation(note)) {
+      frequency = getNoteFrequency(note);
+    } else if (isNumericRepresentation(note)) {
+      frequency = note;
+    } else {
+      throw new Error('Note is neither a string nor a number!');
+    }
+    playedNotes.forEach(note => {
+      if (note.freq === frequency) {
+        note.gain.gain.setTargetAtTime(0, context.currentTime, mergedSettings.fadeTime);
+      }
+    });
+  };
+
+  _init();
+
+  return {
+    init,
+    play,
+    stop,
+    stopAll,
+  };
+}());
+
+module.exports = SimpleSynth;
